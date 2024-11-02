eb2bfea30c4f059e64c47dc5ec14debe2930d4c4
peritissimus <149.kush@gmail.com>
2024-11-02 12:35:48 +0530

    ...

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,8 @@
+from collections import defaultdict, deque
 from typing import Any, Dict, List
-
-import networkx as nx
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
-# add auth
-# add ratelimit
-# add monitoring
-# add analytics
 
 
 class Node(BaseModel):
@@ -28,11 +22,60 @@ class Pipeline(BaseModel):
     edges: List[Edge]
 
 
-def create_graph(data: Pipeline):
-    G = nx.DiGraph()
-    G.add_nodes_from([node.id for node in data.nodes])
-    G.add_edges_from([(edge.source, edge.target) for edge in data.edges])
-    return G
+class Graph:
+    def __init__(self, nodes, edges):
+        self.nodes = set(nodes)
+        self.edges = edges
+        self.graph = defaultdict(list)
+        for from_node, to_node in edges:
+            self.graph[from_node].append(to_node)
+
+    def is_connected(self):
+        if not self.nodes:
+            return True
+
+        neighbor_map = defaultdict(list)
+        for from_node, to_node in self.edges:
+            neighbor_map[from_node].append(to_node)
+            neighbor_map[to_node].append(from_node)
+
+        start_node = list(self.nodes)[0]
+        visited = set()
+        queue = deque([start_node])
+
+        while queue:
+            node = queue.popleft()
+            visited.add(node)
+            for neighbor in neighbor_map[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+        return visited == self.nodes
+
+    def has_cycle(self):
+        visited = set()
+        rec_stack = set()
+
+        def dfs(node):
+            visited.add(node)
+            rec_stack.add(node)
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor):
+                        return True
+                elif neighbor in rec_stack:
+                    return True
+            rec_stack.remove(node)
+            return False
+
+        for node in self.nodes:
+            if node not in visited:
+                if dfs(node):
+                    return True
+        return False
+
+    def is_valid_dag(self):
+        return self.is_connected() and not self.has_cycle()
 
 
 app = FastAPI()
@@ -59,9 +102,13 @@ def read_root():
 @app.post("/pipelines/parse")
 def parse_pipeline(pipeline: Pipeline):
     try:
-        graph = create_graph(pipeline)
+        edges = [(edge.source, edge.target) for edge in pipeline.edges]
+        nodes = [node.id for node in pipeline.nodes]
+
+        graph = Graph(nodes=nodes, edges=edges)
+
         return {
-            "is_dag": nx.is_weakly_connected(graph),
+            "is_dag": graph.is_valid_dag(),
             "num_nodes": len(pipeline.nodes),
             "num_edges": len(pipeline.edges),
         }
